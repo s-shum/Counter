009e0f2c3c2f2737e91bafad439dc68932626d79
s-shum <102037142+s-shum@users.noreply.github.com>
2024-01-20 20:13:28 +0000

    add fix

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,15 +3,16 @@ public class Calculator {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
         System.out.println("Enter the first number:");
         int num1 = scanner.nextInt();
-
+        
         System.out.println("Enter the operation (+, -, *, /):");
         char operator = scanner.next().charAt(0);
-
+        
         System.out.println("Enter the second number:");
         int num2 = scanner.nextInt();
+        scanner.close();
 
         int result;
 
@@ -39,7 +40,6 @@ public class Calculator {
         }
 
         System.out.println("Result: " + result); // Displaying the result
-
 
 
     }
